2a8d8cbfc0e10e7f2cebde56200659958f009b35
Ansruta <75419631+Ansruta@users.noreply.github.com>
2021-08-18 17:12:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,13 @@ require("@firebase/firestore")
 
 const firebaseConfig = {
  //Add configuration here
- 
+  apiKey: "AIzaSyAK4nY-nrAP-ItQtMmO7p4fvUx-i28u5gg",
+    authDomain: "like-dislike-f7463.firebaseapp.com",
+    databaseURL: "https://like-dislike-f7463-default-rtdb.firebaseio.com",
+    projectId: "like-dislike-f7463",
+    storageBucket: "like-dislike-f7463.appspot.com",
+    messagingSenderId: "369703620118",
+    appId: "1:369703620118:web:feece044099fef7fa9af37"
 };
   firebase.initializeApp(firebaseConfig);
 
